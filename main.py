2ee5cd4a201eec8e19dec1e87cecfa358632276c
ij96 <ij96@users.noreply.github.com>
2018-04-01 17:14:37 +0100

    Added RPM related functions - edge counting, speed calculation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 ''' Treadmill speed controller '''
 
 from machine import Pin, PWM
-import utime
+from pyb import Timer
+
+#################### PWM output ################################################
 
 pin_pwm = Pin(12, Pin.OUT)      # create Pin object on pin 12
 pwm = PWM(pin_pwm, freq=100)    # create PWM object, freq. = 400Hz
@@ -20,3 +22,30 @@ pin_0.on()
     18              40                  410
 '''
 
+#################### read RPM by counting edges ################################
+#   2 edges per revolution
+#   diameter of rotor bar = 58.3mm -> 0.3663m travelled per revolution
+#   assuming lowest speed is 0.8km/h = 0.2222m/s = 0.6rps = 36rpm
+#                                       -> 1.2 edges per sec
+#                                       -> counting edges every sec is enough
+#   rps = num. of edges per sec / 2
+#   speed = rps * 0.3663       (unit: m/s)
+#         = rps * 0.3663 * 3.6 (unit: km/h)
+
+speed = 0   # unit: km/h
+
+def rpm_edge_increment():
+    rpm_edge_counter += 1
+
+def update_speed():
+    speed = rpm_edge_counter / 2 * 0.3663 * 3.6
+    rpm_edge_counter = 0                    # clear counter
+    printf('Speed: {} km/h'.format(speed))  # print result
+
+# timer
+tim_rpm = Timer(1, freq=1)                  # trigger every 1 second
+tim_rpm.callback(update_speed)
+
+# hardware interrupt pin
+pin_rpm = Pin(13, Pin.IN)                   # create Pin object on pin 13
+pin_rpm.irq(trigger = Pin.IRQ_FALLING, handler=rpm_edge_increment) # set up IRQ
